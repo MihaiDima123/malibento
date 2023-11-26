b67649ae966c512bc1947347df606c8597e308d4
Mihai Dima <dimamihaimail@gmail.com>
2023-11-26 14:45:47 +0000

    first commit

---
+++ src/test/java/com/malibento/malibento/MalibentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malibento.malibento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalibentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
